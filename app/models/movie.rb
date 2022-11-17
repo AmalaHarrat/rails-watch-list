3a898d15a3e155f7366a101c2012f6cf0c7997ab
Amala Harrat <amal.harrat@hotmail.fr>
2022-11-17 12:42:38 +0100

    model and validations

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, :overview, uniqueness: true
+  validates :title, presence: true
+  validates :overview, presence: true
+
+end
