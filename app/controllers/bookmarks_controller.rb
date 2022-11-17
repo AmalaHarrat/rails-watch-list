bd5bc9231c6744d18d43929cf236fb7d70ad46bf
Amala Harrat <amal.harrat@hotmail.fr>
2022-11-17 16:55:07 +0100

    wish list movies CRU missing D

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
+  root to: "lists#home", as: :home
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  # Defines the root path route ("/")
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# pas besoin de mettre un eroute movie car pas d'action dessus
