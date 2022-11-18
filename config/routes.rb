d21f292ab98bb578142816f18784bc45ee80244b
Amala Harrat <amal.harrat@hotmail.fr>
2022-11-18 13:17:58 +0100

    movie heroku step

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   # root to: "lists#home", as: :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   # Defines the root path route ("/")
